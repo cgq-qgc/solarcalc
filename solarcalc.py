c9bbe72885da4dec6c8bbc47bef291e1cfc4241c
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-11-20 00:09:46 -0500

    Port getET from java to python

--- solarcalc.py
+++ solarcalc.py
@@ -18,3 +18,33 @@
 # Accessed 2021-11-19.
 # -----------------------------------------------------------------------------
 
+from __future__ import annotations
+import numpy as np
+import pandas as pd
+
+
+def getET(dayofyear: int) -> float:
+    """
+    Calculate the ET (equation of time) correction typically a 15-20 minute
+    correction depending on calendar day.
+
+    Parameters
+    ----------
+    dayofyear : int
+        Day of year.
+
+    Returns
+    -------
+    float
+        Calculated ET value.
+
+    """
+    ETcalc = (279.575 + 0.9856 * dayofyear) * np.pi / 180
+
+    return (
+        -104.7 * np.sin(ETcalc) + 596.2 * np.sin(ETcalc * 2) +
+        4.3 * np.sin(3 * ETcalc) + -12.7 * np.sin(4 * ETcalc) +
+        -429.3 * np.cos(ETcalc) + -2.0 * np.cos(2 * ETcalc) +
+        19.3 * np.cos(3 * ETcalc)
+        ) / 3600
+
