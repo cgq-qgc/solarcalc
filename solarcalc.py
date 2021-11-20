0b9becf207b70bfa1bc011eb072cb14c348432fe
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-11-20 00:09:01 -0500

    first commit main file

---
+++ solarcalc.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © INRS
+# https://github.com/cgq-qgc/rsesq-bulletin
+
+# This code is licensed under the terms of the MIT License as published
+# by the Open Source Initiative. For more details, see the MIT license at
+# https://opensource.org/licenses/MIT.
+
+# This file is a derivative work of codes taken from the SolarCalc 1.0 program,
+# licensed under the terms of the Creative Commons CCZero License as published
+# by the Creative Commons nonprofit organization. For more details, see the
+# CC0 1.0 License at https://creativecommons.org/publicdomain/zero/1.0/.
+
+# Source:
+# USDA Agricultural Research Service. (2019). SolarCalc 1.0. United States
+# Department of Agriculture. https://data.nal.usda.gov/dataset/solarcalc-10.
+# Accessed 2021-11-19.
+# -----------------------------------------------------------------------------
+
