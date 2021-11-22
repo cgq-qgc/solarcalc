2e712826436354b317c48f989e7ffbdb1fcc8565
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-11-22 16:35:05 -0500

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © INRS
+# https://github.com/cgq-qgc/solarcalc
+#
+# This code is licensed under the terms of the MIT License as published
+# by the Open Source Initiative. For more details, see the MIT license at
+# https://opensource.org/licenses/MIT.
+# -----------------------------------------------------------------------------
+
+"""Installation script """
+
+import setuptools
+from setuptools import setup
+from solarcalc import __version__, __project_url__
+
+LONG_DESCRIPTION = (
+    "The SolarcCalc module is used to predict hourly global solar "
+    "radiation on a horizontal surface from daily average temperature "
+    "extremes and total precipitation records. "
+    "The method used in the calculations is based on that presented in "
+    "[An Introduction to Environmental Biophysics]"
+    "(https://link.springer.com/book/10.1007/978-1-4612-1626-1) "
+    "by Campbell, G.S. and J.M. Norman (1998)."
+    "<br><br>"
+    "Version 1.0 of the SolarcCalc module is based on the source code of "
+    "SolarCalc.jar (version 1.1, Jan. 2006), a computer program  that was "
+    "developped by the USDA Agricultural Research Service and that is "
+    "available free of charge at "
+    "[https://data.nal.usda.gov/dataset/solarcalc-10]"
+    "(https://data.nal.usda.gov/dataset/solarcalc-10)."
+    "<br><br>"
+    "Subsequent versions of the SolarcCalc module implemented performance "
+    "improvements and fixed some issues with the original code and method."
+    )
+
+INSTALL_REQUIRES = ['numpy, pandas']
+
+setup(name='solarcalc',
+      version=__version__,
+      description=("Hourly global solar radiation estimator."),
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      license='MIT',
+      author='Jean-Sébastien Gosselin',
+      author_email='jean-sebastien.gosselin@outlook.ca',
+      url=__project_url__,
+      ext_modules=[],
+      packages=setuptools.find_packages(),
+      package_data={},
+      include_package_data=True,
+      install_requires=INSTALL_REQUIRES,
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Intended Audience :: Science/Research",
+                   "Intended Audience :: Education",
+                   "Topic :: Scientific/Engineering"
+                   ],
+      )
--- solarcalc.py
+++ solarcalc.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # Copyright © INRS
-# https://github.com/cgq-qgc/rsesq-bulletin
-
+# https://github.com/cgq-qgc/solarcalc
+#
 # This code is licensed under the terms of the MIT License as published
 # by the Open Source Initiative. For more details, see the MIT license at
 # https://opensource.org/licenses/MIT.
-
+#
 # This file is a derivative work of codes taken from the SolarCalc 1.0 program,
 # licensed under the terms of the Creative Commons CCZero License as published
 # by the Creative Commons nonprofit organization. For more details, see the
 # CC0 1.0 License at https://creativecommons.org/publicdomain/zero/1.0/.
-
+#
 # Source:
-# USDA Agricultural Research Service. (2019). SolarCalc 1.0. United States
+# USDA Agricultural Research Service (2019). SolarCalc 1.0. United States
 # Department of Agriculture. https://data.nal.usda.gov/dataset/solarcalc-10.
 # Accessed 2021-11-19.
 # -----------------------------------------------------------------------------
@@ -21,14 +21,16 @@
 """
 Global Solar Radiation Estimator
 
-Predicts hourly global solar radiation on a horizontal surface from only
-average temperature extremes and total precipitation records.
+Predicts hourly global solar radiation on a horizontal surface from
+daily average temperature extremes and total precipitation records.
 """
 
 from __future__ import annotations
-import datetime
 import numpy as np
 import pandas as pd
+
+__version__ = "1.0"
+__project_url__ = "https://github.com/cgq-qgc/solarcalc"
 
 # To avoid "RuntimeWarning: overflow encountered in power" warnings when
 # calculating tau**m, which become very large just outside of the
@@ -310,6 +312,8 @@ def calc_solar_rad(lon_dd: float, lat_dd: float, alt: float,
 
 
 if __name__ == '__main__':
+    import datetime
+
     # To fully compare with SolarCalc.jar, we need to calculate 2019 and 2020
     # separately.
     datafile = "D:/Projets/solarcalc/solarcalc_climatedata_2019.csv"
